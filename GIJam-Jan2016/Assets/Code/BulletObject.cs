91daa7fee5b8a050a97d242ca8780b5c0cd90007
Chyura <mxlxili@gmail.com>
2016-01-30 16:56:22 -0500

    Bullet Files

---
+++ GIJam-Jan2016/Assets/Code/BulletObject.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Ubh bullet.
+/// This is the primary physics stuff is in.
+/// </summary>
+public class BulletObject : Singleton<BulletObject>
+{
+	public int bulletDamage = 10;
+
+	/*
+	void OnDisable ()
+	{
+		StopAllCoroutines();
+		transform.ResetPosition();
+		transform.
+		transform.ResetRotation();
+		_Shooting = false;
+	}
+	*/
+
+
+
+
+	/// <summary>
+	/// Bullet Shot
+	/// </summary>
+	/// 
+	/// 
+
+	public void Shot (float speed, float angle, float accelSpeed, float accelTurn,
+		bool homing, Transform homingTarget, float homingAngleSpeed,
+		bool wave, float waveSpeed, float waveRangeSize)
+	{
+
+		if (gameObject.activeInHierarchy == false) {
+			return;
+		}
+
+		StartCoroutine(MoveCoroutine(speed, angle, accelSpeed, accelTurn,
+			homing, homingTarget, homingAngleSpeed,
+			wave, waveSpeed, waveRangeSize));
+	}
+
+	IEnumerator MoveCoroutine (float speed, float angle, float accelSpeed, float accelTurn,
+		bool homing, Transform homingTarget, float homingAngleSpeed,
+		bool wave, float waveSpeed, float waveRangeSize)
+	{
+
+		Vector3 angleVector = Vector3.zero;
+
+		angleVector.Set(transform.eulerAngles.x, transform.eulerAngles.y, angle);
+
+
+		angleVector.Set(transform.eulerAngles.x, transform.eulerAngles.y, angle);
+		transform.eulerAngles = angleVector;
+
+
+	
+		float selfFrameCnt = 0f;
+		float selfTimeCount = 0f;
+
+
+		while (true) {
+
+			//SPECIFIC BULLLETS -----------------------------------------------------------------------------------------------
+			//HOMING STUFF -----------------------------------------------------
+			if (homing) {
+				// homing target.
+				/*
+				if (homingTarget != null && 0f < homingAngleSpeed) {
+					float rotAngle = ControllerScript.GetAngleFromTwoPosition(transform, homingTarget, axisMove);
+					float myAngle = 0f;
+					if (axisMove == ControllerScript.AXIS.X_AND_Z) {
+						// X and Z axis
+						myAngle = -transform.eulerAngles.y;
+					} else {
+						// X and Y axis
+						myAngle = transform.eulerAngles.z;
+					}
+					
+					float toAngle = Mathf.MoveTowardsAngle(myAngle, rotAngle, GlobalTimerClass.Instance.DeltaTime * homingAngleSpeed);
+					
+					if (axisMove == ControllerScript.AXIS.X_AND_Z) {
+						// X and Z axis
+						transform.SetEulerAnglesY(-toAngle);
+					} else {
+						// X and Y axis
+						transform.SetEulerAnglesZ(toAngle);
+					}
+				}
+				*/
+				//END OF HOMING STUFF ------------------------------------------------
+
+			} else if (wave) {
+
+				//SINE WAVE -----------------------------------
+				/*
+				// acceleration turning.
+				angle += (accelTurn * GlobalTimerClass.Instance.DeltaTime);
+				// wave.
+				if (0f < waveSpeed && 0f < waveRangeSize) {
+					float waveAngle = angle + (waveRangeSize / 2f * Mathf.Sin(selfFrameCnt * waveSpeed / 100f));
+					if (axisMove == ControllerScript.AXIS.X_AND_Z) {
+						// X and Z axis
+						transform.SetEulerAnglesY(-waveAngle);
+					} else {
+						// X and Y axis
+						transform.SetEulerAnglesZ(waveAngle);
+					}
+				}
+				selfFrameCnt++;
+				*/
+				//END OF SINE WAVE
+			} else {
+				// acceleration turning.
+				float addAngle = accelTurn * Time.deltaTime;
+
+				angleVector.Set (angleVector.x, angleVector.y, angleVector.z + addAngle);
+			
+				transform.eulerAngles = angleVector;
+			}
+			//END OF SPECIFIC BULLETS -----------------------------------------------------------------------
+			//STUFF THAT APPLIES TO ALL BULLETS:--------------------------------------------------------------------
+
+			// acceleration speed.
+			speed += (accelSpeed * Time.deltaTime);
+
+	
+				// X and Y axis
+			transform.position += transform.up.normalized * speed * Time.deltaTime;
+			transform.eulerAngles.Set(transform.eulerAngles.x, transform.eulerAngles.y , 5.0f);
+
+			yield return 0;
+
+			//TICKS TIME FOR BULLET TIMEOUT ------------------------------------------------
+			selfTimeCount += Time.deltaTime;
+
+
+	
+		}
+
+
+	}
+
+	public int getBulletDamage(){
+		return bulletDamage;
+	}
+
+}
