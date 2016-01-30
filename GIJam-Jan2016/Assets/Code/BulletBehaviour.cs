9fd11a87b96e850acac106fcda9fc4f975f22398
Chyura <mxlxili@gmail.com>
2016-01-29 20:58:39 -0500

    Bullet code

---
+++ GIJam-Jan2016/Assets/Code/BulletBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+
+	public int bounce = 0;
+	public int bounceLimit = 2;
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	void OnCollisionEnter2D (Collision col) {
+		if (col.gameObject.name == "Wall" && bounce < bounceLimit) {
+			//do bounce thing
+		}
+	}
+			
+			
+}
